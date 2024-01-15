adb9088aa4c9789071bf2ce84c53e599fff05662
Gadzbi <crg1234@gmail.com>
2024-01-15 11:48:00 +0100

    changed printer script to run on suldr driver and added run script for init raspberrypi

--- printer.sh
+++ printer.sh
@@ -3,11 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 echo "Starting printer script"
-echo "Script based on https://www.tomshardware.com/how-to/raspberry-pi-print-server"
+echo "Script based on https://www.bchemnet.com/suldr/index.html and https://www.tomshardware.com/how-to/raspberry-pi-print-server"
+
+echo "Installing external repo for Samsung Drivers"
+sudo bash -c 'echo "deb https://www.bchemnet.com/suldr/ debian extra" >> /etc/apt/sources.list'
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv FB510D557CC3E840 
+gpg --export --armor FB510D557CC3E840 | sudo apt-key add -
+sudo apt update -y
 
 echo "Installing driver for Samsung CLX-3180 and samba"
-sudo apt install printer-driver-foo2zjs samba -y
-# sudo apt install printer-driver-splix
+sudo apt install suld-driver2-1.00.39hp suld-driver2-common-1 samba -y
 
 echo "Adding static IP"
 sudo echo -e "interface wlan0 \nstatic ip_address=192.168.50.99/24 \nstatic routers=192.168.50.1 \nstatic domain_name_servers=192.168.50.1" > /etc/dhcpcd.conf
@@ -23,9 +28,6 @@ sudo systemctl start samba
 echo "Changing /etc/samba/smb.conf"
 sudo cp /etc/samba/smb.conf /etc/samba/smb.bak
 sudo sed -ie 's@\[global\]@\[global\]\nspoolss: architecture = Windows x64@g;s@/var/tmp@/var/spool/samba@g;s@^   guest ok = no@   guest ok = yes@' /etc/samba/smb.conf
-echo "Adding ssh to PI"
-sudo systemctl enable ssh
-sudo systemctl start ssh
 echo "launch browser with address http://raspberrypi:631/ and add a printer (REMEMBER to share it)"
 read;
 echo "Based on https://wiki.ipfire.org/addons/cups"
